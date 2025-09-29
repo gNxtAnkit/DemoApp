aa05f0e432ce72960af497cbaefac2851780924c
Ankit Gupta <ankit@gnxtsystems.com>
2025-09-29 17:03:05 +0530

    dev4 changes

--- DemoApp/Form1.vb
+++ DemoApp/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Button1.Text = "Hello"
+        Button1.Text = "Hello change - dev4"
         Button1.Text = "world"
     End Sub
 
